35a3b4b77053a39d409472322bd98d047d9fc878
BT_1N3_33 <dsad@wds>
2024-10-25 10:03:51 -0400

    modified print statements

--- src/Runner.java
+++ src/Runner.java
@@ -1,15 +1,15 @@
 public class Runner {
     public static void main(String[] args){
         System.out.println("Hello World");
-        System.out.println("Hello Worl");
-        System.out.println("Hello Wor");
-        System.out.println("Hello Wo");
-        System.out.println("Hello W");
-        System.out.println("Hello");
-        System.out.println("Hell");
-        System.out.println("Hel");
-        System.out.println("He");
-        System.out.println("H");
+        System.out.println("Hello Worlx");
+        System.out.println("Hello Worxx");
+        System.out.println("Hello Woxxx");
+        System.out.println("Hello Wxxxx");
+        System.out.println("Hello xxxxx");
+        System.out.println("Hellx xxxxx");
+        System.out.println("Helxx xxxxx");
+        System.out.println("Hexxx xxxxx");
+        System.out.println("Hxxxx xxxxx");
     }
 }
 
