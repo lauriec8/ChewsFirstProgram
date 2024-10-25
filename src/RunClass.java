8cd10b5331d497ee0e716cbfebd06269b5173330
BT_1N3_33 <dsad@wds>
2024-10-25 10:37:13 -0400

    modified print statements to planets

--- src/RunClass.java
+++ src/RunClass.java
@@ -1,8 +1,8 @@
 public class RunClass {
     public static void main(String [] args){
         System.out.println("Hello Mars");
-        System.out.println("Hello Mars");
-        System.out.println("Hello Mars");
-        System.out.println("Hello Mars");
+        System.out.println("Hello Mercury");
+        System.out.println("Hello Jupiter");
+        System.out.println("Hello Saturn");
     }
 }
