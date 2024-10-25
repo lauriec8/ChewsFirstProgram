03a263c1ab1bdbd82753d88563f4b5830d18bf0b
BT_1N3_33 <dsad@wds>
2024-10-25 10:02:47 -0400

    added even more print statements

--- src/Runner.java
+++ src/Runner.java
@@ -1,8 +1,15 @@
 public class Runner {
     public static void main(String[] args){
         System.out.println("Hello World");
+        System.out.println("Hello Worl");
+        System.out.println("Hello Wor");
+        System.out.println("Hello Wo");
+        System.out.println("Hello W");
         System.out.println("Hello");
+        System.out.println("Hell");
+        System.out.println("Hel");
         System.out.println("He");
+        System.out.println("H");
     }
 }
 
