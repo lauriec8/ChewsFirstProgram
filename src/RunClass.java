4900f12e611f14c51996601f0eb71e2099fcd17f
BT_1N3_33 <dsad@wds>
2024-10-25 10:08:07 -0400

    modified print statements

---
+++ src/RunClass.java
@@ -0,0 +1,8 @@
+public class RunClass {
+    public static void main(String [] args){
+        System.out.println("Hello Mars");
+        System.out.println("Hello Mars");
+        System.out.println("Hello Mars");
+        System.out.println("Hello Mars");
+    }
+}
